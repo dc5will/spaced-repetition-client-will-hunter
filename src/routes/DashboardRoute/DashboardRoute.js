15aea350581f489d58330608840f367c67ebca03
William Y Wong <wwong1987@gmail.com>
2019-04-15 12:50:50 -0700

    set up api service for grabbing language for dashboard

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import UserContext from '../../contexts/UserContext';
+import DashboardApiService from '../../services/dashboard-service';
 
 class DashboardRoute extends Component {
+  state = {
+    error: null,
+    language: null,
+  }
+
+  static contextType = UserContext;
+
+  componentDidMount() {
+    DashboardApiService.getLanguage().then(data => {
+      console.log(data.language.name);
+      this.setState({ language: data.language.name })
+    }).catch(res => this.setState({ error: res.error }))
+  }
+
   render() {
+    console.log(this.state.language);
     return (
       <section>
-        implement and style me
+        <h2>{this.state.language}</h2>
       </section>
     );
   }
