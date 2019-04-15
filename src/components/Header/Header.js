d174a2e6e412d71e7c18fb2d0065ff1a8c1729ad
Hunter Kreshock <hkreshock@gmail.com>
2019-04-15 10:22:44 -0700

    initial css added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,47 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import TokenService from '../../services/token-service'
-import UserContext from '../../contexts/UserContext'
-import './Header.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import TokenService from "../../services/token-service";
+import UserContext from "../../contexts/UserContext";
+import "./Header.css";
 
 class Header extends Component {
-  static contextType = UserContext
+  static contextType = UserContext;
 
   handleLogoutClick = () => {
-    this.context.processLogout()
-  }
+    this.context.processLogout();
+  };
 
   renderLogoutLink() {
     return (
       <div>
-        <span>
-          {this.context.user.name}
-        </span>
+        <span>{this.context.user.name}</span>
         <nav>
-          <Link
-            onClick={this.handleLogoutClick}
-            to='/login'>
+          <Link className="logout" onClick={this.handleLogoutClick} to="/login">
             Logout
           </Link>
         </nav>
       </div>
-    )
+    );
   }
 
   renderLoginLink() {
     return (
       <nav>
-        <Link to='/login'>Login</Link>
-        {' '}
-        <Link to='/register'>Sign up</Link>
+        <Link className="login" to="/login">
+          Login
+        </Link>{" "}
+        <Link className="register" to="/register">
+          Sign up
+        </Link>
       </nav>
-    )
+    );
   }
 
   render() {
     return (
       <header>
         <h1>
-          <Link to='/'>
+          <Link className="spacedRepetition" to="/">
             Spaced repetition
           </Link>
         </h1>
@@ -54,4 +53,4 @@ class Header extends Component {
   }
 }
 
-export default Header
+export default Header;
--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,31 +1,36 @@
-import React, { Component } from 'react'
-import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
+import React, { Component } from "react";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+import "./RegistrationRoute.css";
 
 class RegistrationRoute extends Component {
   static defaultProps = {
     history: {
-      push: () => {},
-    },
-  }
+      push: () => {}
+    }
+  };
 
   handleRegistrationSuccess = () => {
-    const { history } = this.props
-    history.push('/login')
-  }
+    const { history } = this.props;
+    history.push("/login");
+  };
 
   render() {
     return (
-      <section>
-        <p>
-          Practice learning a language with the spaced reptition revision technique.
-        </p>
-        <h2>Sign up</h2>
+      <div>
+        <section className="description">
+          <p>
+            Practice learning a language with the spaced reptition revision
+            technique.
+          </p>
+        </section>
+        <h2 className="signup">Sign up</h2>
+
         <RegistrationForm
           onRegistrationSuccess={this.handleRegistrationSuccess}
         />
-      </section>
+      </div>
     );
   }
 }
 
-export default RegistrationRoute
+export default RegistrationRoute;
