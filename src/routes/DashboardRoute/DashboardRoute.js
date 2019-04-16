6333042794b9018502fcde7ea6d3a3c328f47a3f
Hunter Kreshock <hkreshock@gmail.com>
2019-04-16 12:56:36 -0700

    dashboard testing and css

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,51 +1,59 @@
-import React, { Component } from 'react'
-import UserContext from '../../contexts/UserContext';
-import DashboardApiService from '../../services/dashboard-service';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import UserContext from "../../contexts/UserContext";
+import DashboardApiService from "../../services/dashboard-service";
+import { Link } from "react-router-dom";
+import "./DashboardRoute.css";
 
 class DashboardRoute extends Component {
   state = {
     error: null,
     language: null,
-    words: []
-  }
+    words: [],
+    totalScore: null
+  };
 
   static contextType = UserContext;
 
+  calculateTotal() {
+    DashboardApiService.getLanguage().then(data =>
+      this.setState({ totalScore: data.language.total_score })
+    );
+  }
+
   componentDidMount() {
-    // console.log(this.state.words)
-    DashboardApiService.getLanguage().then(data => {
-      console.log(data.language.name);
-      this.setState({ language: data.language.name, words: data.words })
-    }).catch(res => this.setState({ error: res.error }))
+    DashboardApiService.getLanguage()
+      .then(data => {
+        this.setState({ language: data.language.name, words: data.words });
+      })
+      .then(this.calculateTotal())
+      .catch(res => this.setState({ error: res.error }));
   }
 
   createWords() {
     return this.state.words.map(word => {
       return (
-        <li key={word.id}>
-          <h3>{word.original}</h3>
-          <p>correct: {word.correct_count}</p>
-          <p>incorrect: {word.incorrect_count}</p>
+        <li className="eachWord" key={word.id}>
+          <h4>{word.original}</h4>
+          <p className="correctCount">correct answer count: {word.correct_count}</p>
+          <p className="incorrectCount">incorrect answer count: {word.incorrect_count}</p>
         </li>
-      )
-    })
+      );
+    });
   }
 
   render() {
-
-    // console.log(this.state.language.total_score)
     return (
       <section>
         <h2>Learn {this.state.language}</h2>
-        <h3>Words List</h3>
-        <ul>{this.createWords()}</ul>
-        <Link to='/learn'>
-          <button className='begin-button'>Begin</button>
+        <h2>Total correct answers: {this.state.totalScore}</h2>
+        <h3>Words to practice</h3>
+        <ul className="eachWordContainer">{this.createWords()}</ul>
+        <Link to="/learn">
+          <button className="begin-button">Start practicing</button>
         </Link>
       </section>
     );
   }
 }
 
-export default DashboardRoute
+export default DashboardRoute;
