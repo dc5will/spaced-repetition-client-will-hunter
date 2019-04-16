ca15d20f826c61cb584fa92492bc6aa0f8b8f7c9
Hunter Kreshock <hkreshock@gmail.com>
2019-04-16 14:31:47 -0700

    started learning route added css

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,13 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import DashboardApiService from "../../services/dashboard-service";
+import { Link } from "react-router-dom";
 
 class LearningRoute extends Component {
+  state = {
+    error: null,
+    words: [],
+    totalScore: null
+  };
+
+  componentDidMount() {
+    DashboardApiService.getLanguage()
+      .then(data => {
+        this.setState(
+          {
+            words: data.words
+          },
+          function() {
+            console.log(this.state.words);
+          }
+        );
+        return data;
+      })
+      .then(data => {
+        this.setState(
+          {
+            totalScore: data.language.total_score
+          },
+          function() {
+            console.log(this.state.total_score);
+          }
+        );
+      })
+      .catch(res => this.setState({ error: res.error }));
+  }
+
+  createWords() {
+    return this.state.words.map(word => {
+      return (
+        <li className="eachWord" key={word.id}>
+          <h4>{word.original}</h4>
+          <p className="correctCount">
+            correct answer count: {word.correct_count}
+          </p>
+          <p className="incorrectCount">
+            incorrect answer count: {word.incorrect_count}
+          </p>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
-      <section>
-        implement and style me
-      </section>
+      <div>
+        <h2>Total correct answers: {this.state.totalScore}</h2>
+        <ul className="eachWordContainer">{this.createWords()}</ul>
+        <Link to={"/"}>Back to Dashboard</Link>
+      </div>
     );
   }
 }
 
-export default LearningRoute
+export default LearningRoute;
