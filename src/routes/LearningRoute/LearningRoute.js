c464289e1622b9022137679befa58b044f1dc692
Hunter Kreshock <hkreshock@gmail.com>
2019-04-17 09:53:18 -0700

    working learning route

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import DashboardApiService from "../../services/dashboard-api-service";
 import LearningApiService from "../../services/learning-api-service";
 import { Link } from "react-router-dom";
+import "./LearningRoute.css";
 
 class LearningRoute extends Component {
   state = {
     error: null,
-    words: [],
-    totalScore: null
+    words: null,
+    totalScore: null,
+    currentWord: 0
   };
 
   // 1) displays the current score and h2 with next word
@@ -49,27 +51,73 @@ class LearningRoute extends Component {
       .catch(res => this.setState({ error: res.error }));
   }
 
+  handleSubmit = ev => {
+    ev.preventDefault();
+    const { userTranslation } = ev.target;
+
+    this.setState({ error: null });
+    this.setState({ currentWord: this.state.currentWord + 1 });
+
+    LearningApiService.makeGuess({
+      userTranslation: userTranslation.value
+    })
+      .then(res => {
+        userTranslation.value = "";
+      })
+      .catch(res => {
+        this.setState({ error: res.error });
+      });
+  };
+
   createWords() {
-    return this.state.words.map(word => {
+    if (this.state.currentWord < this.state.words.length) {
       return (
-        <li className="eachWord" key={word.id}>
-          <h4>{word.original}</h4>
-          <p className="correctCount">
-            correct answer count: {word.correct_count}
-          </p>
-          <p className="incorrectCount">
-            incorrect answer count: {word.incorrect_count}
-          </p>
+        <li
+          className="eachQuizWord"
+          key={this.state.words[this.state.currentWord].id}
+        >
+          <h4>{this.state.words[this.state.currentWord].original}</h4>
+          <section className="correct_incorrect_count">
+            <p className="correctCount">
+              correct answer count:{" "}
+              {this.state.words[this.state.currentWord].correct_count}
+            </p>
+            <p className="incorrectCount">
+              incorrect answer count:{" "}
+              {this.state.words[this.state.currentWord].incorrect_count}
+            </p>
+          </section>
+          <br />
+          <br />
+          <form className="quiz" onSubmit={this.handleSubmit}>
+            <h2 className="quizAnswer" htmlFor="userTranslation">
+              Translate the word:{" "}
+            </h2>
+            <input
+              type="text"
+              id="userTranslation"
+              className="userTranslationInput"
+            />
+            <br />
+            <br />
+            <button type="submit">See answer</button>
+          </form>
         </li>
       );
-    });
+    } else {
+      return <h2>You're all out of words!</h2>;
+    }
   }
 
   render() {
     return (
       <div>
-        <h2>Total correct answers: {this.state.totalScore}</h2>
-        <ul className="eachWordContainer">{this.createWords()}</ul>
+        <h1 className="totalCorrectAnswers">
+          Total correct answers: {this.state.totalScore}
+        </h1>
+        {this.state.words !== null ? (
+          <ul className="eachQuizWordContainer">{this.createWords()}</ul>
+        ) : null}
         <Link to={"/"}>Back to Dashboard</Link>
       </div>
     );
